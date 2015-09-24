9b1a3ebbc27754099baa2256dbb95a90f55f06af
Dianna Faulk <diannafaulk@gmail.com>
2015-09-24 13:29:19 -0700

    (server) Create basic server, serve index.html

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname+'/client'));
+
+app.get('/', function(req, res){
+  res.send('index.html');
+});
+
+var server = app.listen(3020, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('ArtFiend listening on http://%s:%s', host, port);
+});
