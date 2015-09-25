73a90d88694482ba43dc2cf1bedaefbf07196cb3
Dianna Faulk <diannafaulk@gmail.com>
2015-09-24 17:10:18 -0700

    (test) Include morgan to log requests

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
 
+app.use(morgan('dev'));
 app.use(express.static(__dirname+'/client/'));
 app.use('/bower_components', express.static(__dirname+'/bower_components/'))
+
+
+// app.use('/app', express.static(__dirname+'/client/app/'));
+// app.use('/gallery', express.static(__dirname+'/client/app/gallery/'));
 
 app.get('/', function(req, res){
   res.send('index.html');
